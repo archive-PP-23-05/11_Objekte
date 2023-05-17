ed5e1ebf9f5affd3371ddbffebd297b85a19f12f
Peter <schmidt.multimedia@gmail.com>
2023-05-17 10:18:16 +0200

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -20,6 +20,7 @@
 // output(arr[0]);
 // output(arr[0][0]+ " " + arr[1][2]);
 // output(arr[1][0]);
+// output("------------");
 
 // // nested loop
 // for (let i = 0; i < arr.length; i++) {
@@ -32,10 +33,29 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = {
+               firstName: "Lukas" ,
+               familyName: "Gedrat",
+               salary:[120000, 160000],
+               permission: true,
+               sayHello: 
+                        function(){
+                            return "Hello, " + this.firstName + "!";
+                        }
+            };
 
+output(person);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
 
+const text =    "Ich bin " + person.firstName + 
+                " und verdiene " + person.salary[1] +
+                " p.a.";
 
-
+output(text);
+output(person.sayHello());
 
 
 /** Ausgabe */
